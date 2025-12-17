f07d72a43cf734e510537893d01d175f37bf275b
damir0129 <damir0129@gmail.com>
2025-12-18 08:58:15 +0900

    lesson 8

---
+++ lesson_8/lesson_8_2.py
@@ -0,0 +1,33 @@
+class PrintData:
+    def print(self, data):
+        self.send_data(data)
+        print(f"Print: {data}")
+
+    def send_data(self, data):
+        if not self.send_to_print(data):
+            raise PrintError("Принтер не отвечает", code=503)
+
+    def send_to_print(self, data):
+        return False
+
+
+class PrintError(Exception):
+    """Ошибка печати"""
+    def __init__(self, message, code=None):
+        self.message = message
+        self.code = code
+
+    def __str__(self):
+        return f"[Ошибка {self.code}] {self.message}" \
+            if self.code else self.message
+
+
+class PrintConnectionError(PrintError):
+    pass
+
+p = PrintData()
+
+try:
+    p.send_data("Отчет")
+except PrintError as e:
+    print(e)
